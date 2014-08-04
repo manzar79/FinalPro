669802aa870ad4c8bc35b1469c8244d6945427b6
manzar79 <manzar79@gmail.com>
2014-08-04 11:57:45 -0400

    Shared to all

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalPro.Startup))]
+namespace FinalPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
