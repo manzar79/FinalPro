86af0c7dc2d31fa16094518e09c15d344288e09d
manzar79 <manzar79@gmail.com>
2014-08-05 14:40:34 -0400

    Added rest of  the Controllers

---
+++ MeatMe/MeatMe/Controllers/AltNameController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MeatMe.Models;
+
+namespace MeatMe.Controllers
+{
+    public class AltNameController : Controller
+    {
+        private FinalProjectDBEntities1 db = new FinalProjectDBEntities1();
+
+        // GET: AltName
+        public ActionResult Index()
+        {
+            var altNames = db.AltNames.Include(a => a.CutName);
+            return View(altNames.ToList());
+        }
+
+        // GET: AltName/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AltName altName = db.AltNames.Find(id);
+            if (altName == null)
+            {
+                return HttpNotFound();
+            }
+            return View(altName);
+        }
+
+        // GET: AltName/Create
+        public ActionResult Create()
+        {
+            ViewBag.CutId = new SelectList(db.CutNames, "CutId", "CutName1");
+            return View();
+        }
+
+        // POST: AltName/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AltNameId,CutId,AltName1")] AltName altName)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AltNames.Add(altName);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CutId = new SelectList(db.CutNames, "CutId", "CutName1", altName.CutId);
+            return View(altName);
+        }
+
+        // GET: AltName/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AltName altName = db.AltNames.Find(id);
+            if (altName == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CutId = new SelectList(db.CutNames, "CutId", "CutName1", altName.CutId);
+            return View(altName);
+        }
+
+        // POST: AltName/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AltNameId,CutId,AltName1")] AltName altName)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(altName).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CutId = new SelectList(db.CutNames, "CutId", "CutName1", altName.CutId);
+            return View(altName);
+        }
+
+        // GET: AltName/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AltName altName = db.AltNames.Find(id);
+            if (altName == null)
+            {
+                return HttpNotFound();
+            }
+            return View(altName);
+        }
+
+        // POST: AltName/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AltName altName = db.AltNames.Find(id);
+            db.AltNames.Remove(altName);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MeatMe/MeatMe/Controllers/CutNameController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MeatMe.Models;
+
+namespace MeatMe.Controllers
+{
+    public class CutNameController : Controller
+    {
+        private FinalProjectDBEntities1 db = new FinalProjectDBEntities1();
+
+        // GET: CutName
+        public ActionResult Index()
+        {
+            var cutNames = db.CutNames.Include(c => c.PrimalCut);
+            return View(cutNames.ToList());
+        }
+
+        // GET: CutName/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CutName cutName = db.CutNames.Find(id);
+            if (cutName == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cutName);
+        }
+
+        // GET: CutName/Create
+        public ActionResult Create()
+        {
+            ViewBag.PrimalCutId = new SelectList(db.PrimalCuts, "PrimalCutId", "PrimalCutName");
+            return View();
+        }
+
+        // POST: CutName/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CutId,CutName1,PrimalCutId")] CutName cutName)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CutNames.Add(cutName);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PrimalCutId = new SelectList(db.PrimalCuts, "PrimalCutId", "PrimalCutName", cutName.PrimalCutId);
+            return View(cutName);
+        }
+
+        // GET: CutName/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CutName cutName = db.CutNames.Find(id);
+            if (cutName == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PrimalCutId = new SelectList(db.PrimalCuts, "PrimalCutId", "PrimalCutName", cutName.PrimalCutId);
+            return View(cutName);
+        }
+
+        // POST: CutName/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CutId,CutName1,PrimalCutId")] CutName cutName)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cutName).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PrimalCutId = new SelectList(db.PrimalCuts, "PrimalCutId", "PrimalCutName", cutName.PrimalCutId);
+            return View(cutName);
+        }
+
+        // GET: CutName/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CutName cutName = db.CutNames.Find(id);
+            if (cutName == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cutName);
+        }
+
+        // POST: CutName/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CutName cutName = db.CutNames.Find(id);
+            db.CutNames.Remove(cutName);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
